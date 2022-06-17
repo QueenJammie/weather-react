5be2ca10f1096487422afd1fb5c0cc5a85834267
QueenJammie <jammie.lacc@outlook.com>
2022-06-16 22:19:28 -0400

    Added a signature but remove the footer

--- src/App.js
+++ src/App.js
@@ -12,10 +12,8 @@ export default function App() {
       <header className="App-header">
         <h1 class="mt-3 ms-2"><img src={logo} className="App-logo" alt="logo" width="50px" />React Weather app.</h1>
         <Weather city="Sherbrooke" />
+          <p><a href="https://github.com/QueenJammie/weather/tree/master/src">Open-source code</a> by <a href="https://www.shecodes.io/hall-of-fame?q=Jammie+Lachance-C%C3%B4t%C3%A9&commit=Search">Jammie Lachance-Côté</a></p>
       </header>
-      <body>
-        <footer>g</footer>
-      </body>
     </div>
   );
 }
