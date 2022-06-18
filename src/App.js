59bdaf2cf3169c00a0bac161fb21fb3acb6e217b
QueenJammie <jammie.lacc@outlook.com>
2022-06-17 20:37:05 -0400

    Added the forecast

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import React from "react";
 import ReactDOM from "react-dom";
 import Weather from "./Weather";
+import Forecast from "./Forecast";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
@@ -12,7 +13,7 @@ export default function App() {
     <div className="container">
       <div className="App">
         <div className="row">
-          <h1 class="mt-3 ms-2"><img src={logo} className="App-logo" alt="logo" width="50px" />React Weather app.</h1>
+          <h1 class="ms-2"><img src={logo} className="App-logo" alt="logo" width="50px" />React Weather app.</h1>
         </div>
           <div class="row">
           <div class="col-6">
@@ -26,7 +27,9 @@ export default function App() {
           </div>
         </div>
           <Weather city="Sherbrooke" />
-          <p><a href="https://github.com/QueenJammie/weather/tree/master/src">Open-source code</a> by <a href="https://www.shecodes.io/hall-of-fame?q=Jammie+Lachance-C%C3%B4t%C3%A9&commit=Search">Jammie Lachance-Côté</a></p>
+          <Forecast city="Sherbrooke"className="mt-5" />
+
+          <p><a href="https://github.com/QueenJammie/weather-react/tree/main/src">Open-source code</a> by <a href="https://www.shecodes.io/hall-of-fame?q=Jammie+Lachance-C%C3%B4t%C3%A9&commit=Search">Jammie Lachance-Côté</a></p>
           
       </div>
     </div>
---
+++ src/Forecast.js
@@ -0,0 +1,37 @@
+import React from "react";
+import sunnyclouds from "../src/images/sunny-clouds.png";
+import storms from "../src/images/tstorms.png";
+import sun from "../src/images/sun.png";
+
+export default function Forecast()
+{
+    return (
+        <div className="row mt-5">
+            <div className="col-2 forecast">
+                <p>06 / 17<br />
+                Sunny cloudy</p>
+                <p><img className="forecastIcon" src={sunnyclouds} alt="Sunny Clouds" /></p>
+            </div>
+            <div className="col-2 forecast">
+                <p>06 / 17<br />
+                Storms</p>
+                <p><img className="forecastIcon" src={storms} alt="Storms" /></p>
+            </div>
+            <div className="col-2 forecast">
+                <p>06 / 17<br />
+                Sunny cloudy</p>
+                <p><img className="forecastIcon" src={sunnyclouds} alt="Sunny Clouds" /></p>
+            </div>
+            <div className="col-2 forecast">
+                <p>06 / 17<br />
+                Sun</p>
+                <p><img className="forecastIcon" src={sun} alt="Sun" /></p>
+            </div>
+            <div className="col-2 forecast">
+                <p>06 / 17<br />
+                Sun</p>
+                <p><img className="forecastIcon" src={sun} alt="Sun" /></p>
+            </div>
+        </div>
+    );
+}
