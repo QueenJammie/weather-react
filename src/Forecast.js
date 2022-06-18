514ce88871dc9e182837a2e3a7bc5b5ec9a0183d
QueenJammie <jammie.lacc@outlook.com>
2022-06-17 21:50:59 -0400

    Added the days of the week

--- src/Forecast.js
+++ src/Forecast.js
@@ -8,27 +8,32 @@ export default function Forecast()
     return (
         <div className="row mt-5">
             <div className="col-2 forecast">
-                <p>06 / 18<br />
+                <p>SAturday<br />
+                06 / 18<br />
                 Sunny cloudy</p>
                 <p><img className="forecastIcon" src={sunnyclouds} alt="Sunny Clouds" /></p>
             </div>
             <div className="col-2 forecast">
-                <p>06 / 19<br />
+                <p>Sunday<br />
+                06 / 19<br />
                 Storms</p>
                 <p><img className="forecastIcon" src={storms} alt="Storms" /></p>
             </div>
             <div className="col-2 forecast">
-                <p>06 / 20<br />
+                <p>Monday<br />
+                06 / 20<br />
                 Sunny cloudy</p>
                 <p><img className="forecastIcon" src={sunnyclouds} alt="Sunny Clouds" /></p>
             </div>
             <div className="col-2 forecast">
-                <p>06 / 21<br />
+                <p>Thuesday<br />
+                06 / 21<br />
                 Sun</p>
                 <p><img className="forecastIcon" src={sun} alt="Sun" /></p>
             </div>
             <div className="col-2 forecast">
-                <p>06 / 22<br />
+                <p>Wednesday<br />
+                06 / 22<br />
                 Sun</p>
                 <p><img className="forecastIcon" src={sun} alt="Sun" /></p>
             </div>
