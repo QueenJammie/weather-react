282397fe944d966cfc55177d18a6372c1d05cd4b
QueenJammie <jammie.lacc@outlook.com>
2022-06-20 11:57:36 -0400

    Made all the informations real in real time

--- src/Weather.js
+++ src/Weather.js
@@ -8,23 +8,29 @@ import brokenClouds from "../src/images/sunny-clouds.png";
 
 export default function Weather(props) {
   let [loaded, setLoaded] = useState(false);
-  let [weatherData, setWeatherData] = useState({});
+  let [weatherData, setWeatherData] = useState({loaded : false});
   //let [city, setCity] = useState("null");
 
   function showTemperature(response) {
     console.log(response.data);
     setWeatherData({
+      loaded: true,
       temperature: Math.round(response.data.main.temp),
-      wind: 3,
+      tempMin: response.data.main.temp_min,
+      tempMax: response.data.main.temp_max,
+      wind: Math.round(response.data.wind.speed),
       name: response.data.name,
-      
+      main: response.data.weather[0].main,
+      description: response.data.weather[0].description,
+      humidity: response.data.main.humidity,
+      iconUrl: ``,
     })
     setLoaded(true);
   }
 
   function farenheitConversion()
   {
-    return "";
+    return null;
   }
 
   if (loaded) {
@@ -34,22 +40,22 @@ export default function Weather(props) {
         <div className="row">
           <div className="col-3">
             <img className="icon" src={brokenClouds} alt="Broken Clouds" />
-            <span className="temperature">10</span>
-            <span className="unit">°C | <a href={farenheitConversion} class="farenheitConversion">°F</a></span>
-            <div className="currentDayWeather"><span className="Minimum">9</span><span className="maximum">15</span></div>
+            <span className="temperature">{weatherData.temperature}</span>
+            <span className="unit">°C/<a href={farenheitConversion} class="farenheitConversion">°F</a></span>
+            <div className="currentDayWeather"><span className="Minimum">{weatherData.tempMin}</span><span className="maximum">{weatherData.tempMax}</span></div>
           </div>
           <div className="col-3 mt-2 weather2">
             <ul>
-              <li>Clouds</li>
-              <li>Humidity: 92%</li>
-              <li>Wind: 10 km/h</li>
+              <li>{weatherData.main}</li>
+              <li>Humidity: {weatherData.humidity}%</li>
+              <li>Wind: {weatherData.wind} km/h</li>
             </ul>
           </div>
           <div className="col-3 ms-5 mt-2">
             <ul className="overview">
-              <li className="actual-city">Sherbrooke</li>
+              <li className="actual-city">{weatherData.name}</li>
               <li>Friday 15:57</li>
-              <li>broken clouds</li>
+              <li className="text-capitalize">{weatherData.description}</li>
             </ul>
           </div>
         </div>
@@ -62,6 +68,5 @@ export default function Weather(props) {
       axios.get(apiUrl).then(showTemperature);
 
       return <p><ThreeDots color="#00BFFF" height={80} width={80} class="center" /></p>;
-    
   }
 }
