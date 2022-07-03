0752b41ba9e22c70aaf396af386c6bcdd10f3ade
QueenJammie <jammie.lacc@outlook.com>
2022-07-02 21:14:29 -0400

    Made the footer in white for better visibility

--- src/App.js
+++ src/App.js
@@ -20,9 +20,9 @@ export default function App() {
           <Weather defaultCity="Sherbrooke" />
           </div>
         </div>
-          <footer>
-            <p><a href="https://github.com/QueenJammie/weather-react/tree/main/src" target="_blank" rel="noopener noreferrer">Open-sourced code</a> by <a href="https://www.shecodes.io/hall-of-fame?q=Jammie+Lachance-C%C3%B4t%C3%A9&commit=Search" target="_blank" rel="noopener noreferrer">Jammie Lachance-Côté</a></p>
-          </footer>    
+          <footer className="footer mt-2">
+            <p><a href="https://github.com/QueenJammie/weather-react/tree/main/src" target="_blank" rel="noopener noreferrer"><strong>Open-source code</strong></a> by <a href="https://www.shecodes.io/hall-of-fame?q=Jammie+Lachance-C%C3%B4t%C3%A9&commit=Search" target="_blank" rel="noopener noreferrer"><strong>Jammie Lachance-Côté</strong></a></p>
+          </footer>
         </body>
       </div>
     </div>
